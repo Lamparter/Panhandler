09de2cadef6a1956dae987730a59d4fb4c59624b
Linus Schneeberg <linus.schneeberg@gmail.com>
2026-07-25 18:57:37 +0200

    Implement liquids

--- src/Riverside.Panhandler/scripts/Kitchen.cs
+++ src/Riverside.Panhandler/scripts/Kitchen.cs
@@ -7,7 +7,17 @@ public partial class Kitchen : Node3D
 	public override void _Ready()
 	{
 		Input.MouseMode = Input.MouseModeEnum.Captured;
-		Input.WarpMouse(new Vector2(0, 0));
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		if (@event.IsActionPressed("unfocus"))
+		{
+			Input.MouseMode =
+				Input.MouseMode == Input.MouseModeEnum.Captured
+					? Input.MouseModeEnum.Visible
+					: Input.MouseModeEnum.Captured;
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
--- src/Riverside.Panhandler/scripts/LiquidDispenser.cs
+++ src/Riverside.Panhandler/scripts/LiquidDispenser.cs
@@ -6,7 +6,8 @@ public partial class LiquidDispenser : Node3D
 	private GpuParticles3D _milkParticles;
 
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready() { 
+	public override void _Ready()
+	{
 		_milkParticles = GetNode<GpuParticles3D>("LiquidEmitter");
 	}
 
